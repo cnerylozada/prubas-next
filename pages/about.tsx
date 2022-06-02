0ef903b882b9532ca50c51f523f1e00beb788879
cnerylozada <cnery@dynamicdevs.io>
2022-06-02 17:12:53 -0500

    feat: client side

--- pages/about.tsx
+++ pages/about.tsx
@@ -14,20 +14,31 @@ const About = ({ staff }: { staff: any }) => {
   // }, []);
   return (
     <>
-      <div>
+      <div className="space-y-10">
         <div className="text-lg font-bold text-blue-500">About</div>
         <div>
           <p>Notes</p>
           <ul className="list-inside list-disc">
+            <li>
+              Static Generation is the pre-rendering method that generates the
+              HTML at build time. The pre-rendered HTML is then reused on each
+              request.
+            </li>
             <li>
               getStaticProps page: el json de la data cargada se genera al
               <span className="font-bold">inicio de la app</span> (ver
               inspector), es por eso q la data y la pagina se cargan cai de
               inmediato cuando entras en ella
             </li>
+            <li>
+              El View page source muestra un html con la data statica y con la
+              data retornada del servicio llamado
+            </li>
+            <li>En Fetch/XHR se carga el .json</li>
           </ul>
         </div>
         <div>
+          <div>Data:</div>
           {!staff && <p>Loading ...</p>}
           {!!staff &&
             staff.map((_: any, index: number) => <p key={index}>{_.name}</p>)}
--- pages/random.tsx
+++ pages/random.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
+import { getStaff } from "../services/staff";
+
 const Random = () => {
+  const [staff, setStaff] = useState<any>();
+  useEffect(() => {
+    getStaff().then((_) => setStaff(_));
+  }, []);
+
   return (
-    <>
+    <div className="space-y-10">
+      <div className="text-lg font-bold text-blue-500">Random</div>
       <div>
-        <div className="text-lg font-bold text-blue-500">Random</div>
+        <p>Notes</p>
+        <ul className="list-inside list-disc">
+          <li>
+            El View page source muestra un html con data statica sin la data
+            retornada del servicio a llamado
+          </li>
+          <li>En Fetch/XHR se carga se invoca el servicio</li>
+        </ul>
       </div>
-    </>
+      <div>
+        <div>Data:</div>
+        {!staff && <p>Loading ...</p>}
+        {!!staff &&
+          staff.map((_: any, index: number) => <p key={index}>{_.name}</p>)}
+      </div>
+    </div>
   );
 };
 
