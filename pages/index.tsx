f3d5f6b0af352b652736ff8d9a4cfd0d76d14c42
cnerylozada <cnery@dynamicdevs.io>
2022-07-17 08:50:50 -0500

    feat: qurable-menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 
+const Wrapper = () => {
+  return (
+    <div
+      className="wrapper absolute w-full h-full top-0 left-0
+      p-4 bg-black bg-opacity-40 "
+    >
+      <div className="dropdown relative inline-block">
+        <div className="w-[40px] h-[40px] bg-green-300 cursor-pointer"></div>
+        <div
+          className={`dropdown-content absolute w-[120px] text-left bg-blue-300`}
+        >
+          <div onClick={() => console.log("option 1")}>Link1</div>
+          <div onClick={() => console.log("option 2")}>Link2</div>
+          <div onClick={() => console.log("option 3")}>Link3</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Home = () => {
   return (
     <>
@@ -12,6 +32,12 @@ const Home = () => {
 
       <div className="space-y-10">
         <div className="text-lg font-bold text-blue-500">Home page</div>
+
+        <div className="mainContainer relative max-w-fit bg-purple-300">
+          <div className="w-[400px] h-[250px] bg-yellow-300"></div>
+          <Wrapper />
+        </div>
+
         <div>
           <p>Notes</p>
           <ul className="list-inside list-disc">
@@ -37,7 +63,6 @@ const Home = () => {
             <div className="bg-gray-200"></div>
           </div>
         ))}
-
         <div className="text-lg font-bold text-blue-500">Art Gallery</div>
         <div className="space-y-5">
           <img
